400ca18e0d221b6348d4e59009b8aee33ab7cc27
Ivanildo de Santana Dantas <ivandant9@gmail.com>
2026-08-22 11:55:48 -0300

    Fix: Avaliacao de formulas do Excel e otimizacao de memoria para o Render

--- src/main/java/br/com/ivanildo/tms/repository/CarregamentoRepository.java
+++ src/main/java/br/com/ivanildo/tms/repository/CarregamentoRepository.java
@@ -4,6 +4,9 @@ import br.com.ivanildo.tms.model.Carregamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CarregamentoRepository extends JpaRepository<Carregamento, Long> {
-}
+    Optional<Carregamento> findByViagem(String viagem);
+}   
