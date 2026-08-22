2aee372675a0f53b1033edbc7f74a46e9befb374
Ivanildo de Santana Dantas <ivandant9@gmail.com>
2026-08-22 09:32:24 -0300

    Chore: remove pasta target do rastreamento do Git

--- target/dev-bundle/webapp/VAADIN/build/vaadin-vertical-layout-ad4174c4-DlrSPLkj.js
+++
@@ -1,1 +0,0 @@
-import{I as a}from"./indexhtml-Du8jT2UJ.js";const o={tagName:"vaadin-vertical-layout",displayName:"Vertical Layout",elements:[{selector:"vaadin-vertical-layout",displayName:"Layout",properties:[a.backgroundColor,a.borderColor,a.borderWidth,a.borderRadius,a.padding,a.gap]}]};export{o as default};
