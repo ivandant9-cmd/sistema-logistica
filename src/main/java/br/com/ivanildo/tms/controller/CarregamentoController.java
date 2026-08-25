4c2e34058fadbb7c38560bf81ec162006db4728f
Ivanildo de Santana Dantas <ivandant9@gmail.com>
2026-08-25 18:02:07 -0300

    fix: corrige erro de sintaxe e limpa endpoints de arquivamento

---
+++ src/main/java/br/com/ivanildo/tms/controller/CarregamentoController.java
@@ -0,0 +1,22 @@
+package br.com.ivanildo.tms.controller;
+
+import br.com.ivanildo.tms.model.Carregamento;
+import br.com.ivanildo.tms.repository.CarregamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/carregamentos")
+public class CarregamentoController {
+
+    @Autowired
+    private CarregamentoRepository carregamentoRepository;
+
+    // Retorna todos os carregamentos cadastrados
+    @GetMapping
+    public List<Carregamento> listarTodos() {
+        return carregamentoRepository.findAll();
+    }
+}
